d7127d35564a345ed2927fa3c7fc97273bbc7b86
Nampy <at_ben@encs.concordia.ca>
2018-07-09 23:01:54 -0400

    added the procedure how to code this

--- src/com/company/JomoPipi.java
+++ src/com/company/JomoPipi.java
@@ -1,5 +1,9 @@
 package com.company;
 
+import javax.print.DocFlavor;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class JomoPipi {
 
     private JomoPipi(){
@@ -7,6 +11,14 @@ public class JomoPipi {
     }
     public static String numericals(String s) {
         //given an input string
+        // if the string is the first character in the string, then replace it with a '1' else have the number of occurrences
+        HashMap<String, Integer> stroeValues = new HashMap<>();
+        for(int i = 0;i< s.length(); i ++){
+            //iterate through the characters in the string
+            // if key does not exist, ad key and set value to 1
+            // else, increase the value by 1
+            // after map the key's value to string's characters
+        }
         return "";
     }
 }
