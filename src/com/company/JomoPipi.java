7bb691612ccbe581b729c63d9d70486524521e9b
Nampy <at_ben@encs.concordia.ca>
2018-07-09 22:56:17 -0400

    Initial commit

---
+++ src/com/company/JomoPipi.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class JomoPipi {
+
+    private JomoPipi(){
+        // no initial constructor instantiation
+    }
+    public static String numericals(String s) {
+        //given an input string
+        return "";
+    }
+}
